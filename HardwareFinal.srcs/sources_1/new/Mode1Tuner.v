174616e2623014d93ba13b77098847279cbeeee9
LeinadV <leinadv86@gmail.com>
2017-01-09 23:16:44 +0800

    fix little bug

--- HardwareFinal.srcs/sources_1/new/Mode1Tuner.v
+++ HardwareFinal.srcs/sources_1/new/Mode1Tuner.v
@@ -65,6 +65,13 @@ module Tuner(
 	
 	always @(*) begin
 		nh = h;
+		case(h_in)
+			5'b00001: nh = 3'b100;
+			5'b00010: nh = 3'b011;
+			5'b00100: nh = 3'b010;
+			5'b01000: nh = 3'b001;
+			5'b10000: nh = 3'b000;
+		endcase
 		if(high && (freq == B)) begin
 			if(h == 3'd4) nh = 3'd0;
 			else nh = h+1'b1;
@@ -73,13 +80,5 @@ module Tuner(
 			if(h == 3'd0) nh = 3'd4;
 			else nh = h-1'b1;
 		end
-		case(h_in)
-			5'b00001: nh = 3'b100;
-			5'b00010: nh = 3'b011;
-			5'b00100: nh = 3'b010;
-			5'b01000: nh = 3'b001;
-			5'b10000: nh = 3'b000;
-			default: nh = h;
-		endcase
 	end
 endmodule
